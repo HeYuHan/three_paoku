d4ca7122517690b856239ae1ac8395e99cbd43c0
HAJ <774985015@qq.com>
2018-03-28 00:42:46 +0800

    audio

--- gaming_dom.js
+++ gaming_dom.js
@@ -3,11 +3,14 @@ function set_pause(){
     var pause_over = get_node('pause_over');
     var pause_again = get_node('pause_again');
     pause_over.addEventListener(touchstart,()=>{
-        window.location.href = 'home.html';
+        play_audio('audio_ok');
+        window.location.href = 'index.html';
     })
 
     pause_again.addEventListener(touchstart,()=>{
+        play_audio('audio_ok');
         hidden_node('pause');
+        play_loop();
         gaming = true;
     })
 }
@@ -19,16 +22,20 @@ function set_over(){
     
     
     over_over.addEventListener(touchstart,()=>{
-        window.location.href = 'home.html';
-    })
-
-    over_again.addEventListener(touchstart,()=>{
+        play_audio('audio_ok');
         window.location.href = 'index.html';
     })
 
+    over_again.addEventListener(touchstart,()=>{
+        play_audio('audio_ok');
+        window.location.href = 'gaming.html';
+    })
+
     over_relife.addEventListener(touchstart,()=>{
+        play_audio('audio_ok');
         hidden_node('over');
         add_oil(PlayScoreCofing.oil);
+        play_loop();
         gaming = true;
     })
 }
@@ -42,4 +49,80 @@ function set_score(){
 function update_oil_img(value){
     var oil = get_node('oil_num');
     oil.style.backgroundSize=value +"% 100%";
+}
+
+function play_audio_main(){
+    var main = get_node('audio_b');
+    var rn = get_node('audio_run_n');
+    play_audio_auto([main]);
+}
+
+function play_audio_auto(audios) {
+    return ;
+        if (window.WeixinJSBridge) {
+            WeixinJSBridge.invoke('getNetworkType', {}, function (e) {
+                audios.forEach(element => {
+                    element.play();
+                });
+            }, false);
+        } else {
+            document.addEventListener("WeixinJSBridgeReady", function () {
+                WeixinJSBridge.invoke('getNetworkType', {}, function (e) {
+                    audios.forEach(element => {
+                        element.play();
+                    });
+                });
+            }, false);
+        }
+        audios.forEach(element => {
+            element.play();
+        });
+    return false;
+}
+
+function play_engine(hi){
+    var name = '',pause = '';
+    if(hi){
+        //高速
+        name = 'audio_run_a';
+        pause = 'audio_run_n';
+    }else{
+        //低速
+        name = 'audio_run_n';
+        pause = 'audio_run_a';
+    }
+    var audio = get_node(name);
+    var pause = get_node(pause);
+
+    if(!audio.currentTime){
+        audio.play();
+        pause.pause();
+    }
+
+}
+
+function pause_loop(){
+    var main = get_node('audio_b');
+    var rn = get_node('audio_run_n');
+    var ra = get_node('audio_run_a');
+    main.pause();
+    rn.pause();
+    ra.pause();
+}
+
+function play_loop(){
+    var main = get_node('audio_b');
+    main.play();
+}
+
+function play_audio(name){
+    var audio = get_node(name);
+    audio.load();
+    audio.pause();
+    audio.play();
+}
+
+function pause_audio(naem){
+    var audio = get_node(name);
+    audio.pause();
 }
--- home_dom.js
+++ home_dom.js
@@ -2,5 +2,5 @@ var touchstart = "touchstart" in window ? "touchstart" : "click";
 var go = get_node('go');
 
 go.addEventListener(touchstart,()=>{
-    window.location.href = 'index.html'
+    window.location.href = 'gaming.html'
 })
